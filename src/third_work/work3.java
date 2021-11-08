babcc35a0fbf06ce90917bfe3c7828bc6fa599f0
qazasd7531 <1946476036@qq.com>
2021-11-08 18:18:40 +0800

    第三次作业

---
+++ src/third_work/work3.java
@@ -0,0 +1,83 @@
+package third_work;
+
+import java.util.*;
+
+public class work3 {
+
+	public static void main(String[] args) {
+		//7.3
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter the integers between 1 and 100:");
+//		int[] t = new int[1010];
+//		while(true) {
+//			int b = input.nextInt();
+//			if(b==0)break;
+//			t[b]++;
+//		}
+//		for(int i=1;i<=100;i++) {
+//			if(t[i]>0) {
+//				System.out.printf(i + " occurs " + t[i] + " times\n");
+//			}
+//		}
+		
+		//7.5
+//		int[] t = new int[110];
+//		int[] a = new int[110];
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter 10 numbers:");
+//		int sum = 0;
+//		for(int i=0;i<10;i++) {
+//			a[i]=input.nextInt();
+//			if(t[a[i]]==0)sum++;
+//			t[a[i]]++;
+//		}
+//		System.out.println("The number of distinct numbers is "+ sum);
+//		System.out.print("The distinct numbers are:");
+//		for(int i=0;i<10;i++) {
+//			if(t[a[i]]>0)System.out.printf(" %d", a[i]);
+//			t[a[i]]=0;
+//		}
+		
+		//7.17
+		
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("输入学生个数");
+//		int n = input.nextInt();
+//		
+//		for(int i=0;i<n;i++) {
+//			
+//		}
+		
+		
+		//8.4
+		//8.6
+		//8.8
+		
+		//8.11
+//		System.out.print("Enter a number between 0 and 511:");
+//		Scanner input = new Scanner(System.in);
+//		int n = input.nextInt();
+//		int[] a = new int[11];
+//		for(int i=8;i>=0;i--) {
+//			a[i] = n%2;
+//			n/=2;
+//		}
+//		for(int i=0;i<9;i++) {
+//			if(a[i]==0) {
+//				System.out.print("H");
+//			}else {
+//				System.out.print("T");
+//			}
+//			if((i+1)%3==0) {
+//				System.out.println();
+//			}else {
+//				System.out.print(" ");
+//			}
+//		}
+		
+		
+	}
+
+}
